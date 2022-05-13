2509cacfe9081e5b668db6ff4d2b906f5493c176
hui mao <409788696@qq.com>
2022-05-13 17:29:16 +0800

    Initial commit

---
+++ client.py
@@ -0,0 +1,35 @@
+"""
+@author:maohui
+@time:2022/5/10 11:49
+"""
+
+
+from socket import *
+
+IP = '192.168.30.43'
+SERVER_PORT = 13333
+BUFLEN = 1024
+
+# 实例化一个socket对象，指明协议
+dataSocket = socket(AF_INET, SOCK_STREAM)
+
+# 连接服务端socket
+dataSocket.connect((IP, SERVER_PORT))
+
+while True:
+    # 从终端读入用户输入的字符串
+    toSend = input('>>> ')
+    if  toSend =='exit':
+        break
+    # 发送消息，也要编码为 bytes
+    dataSocket.send(toSend.encode())
+
+    # 等待接收服务端的消息
+    recved = dataSocket.recv(BUFLEN)
+    # 如果返回空bytes，表示对方关闭了连接
+    if not recved:
+        break
+    # 打印读取的信息
+    print(recved.decode())
+
+dataSocket.close()
